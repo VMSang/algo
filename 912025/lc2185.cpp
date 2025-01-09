44b40bfa7e3e88d0a1333ee8e18992a74e5029f5
VMSang <vosang20102005@gmail.com>
2025-01-09 21:33:43 +0700

    update

---
+++ 912025/lc2185.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+        for(string s : words){
+            if(pref == s.substr(0, pref.size())){
+                cnt++;
+            }
+        }
+        return cnt++;
+    }
+};
